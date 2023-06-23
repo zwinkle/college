c5eff10b31a9e3b6432ca485acbd75b48627aced
Zw <danarwaevolution2@gmail.com>
2023-06-23 12:48:35 +0700

    update commit tugas akhir [23/06/2023]

--- app.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from contact_app import ContactApp
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = ContactApp()
-    window.show()
-    sys.exit(app.exec_())
